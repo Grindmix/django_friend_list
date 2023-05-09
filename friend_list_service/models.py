f62ea584b4a1357da8b2926a3e1016838ac00290
Grindmix <71901283+Grindmix@users.noreply.github.com>
2023-05-09 22:04:25 +0300

    Create Models User, Requests

--- friend_list_service/models.py
+++ friend_list_service/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class User(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(max_length=20)
+    friend_list = models.ManyToManyField("self", blank=True)
+
+
+class Requests(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    from_user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='outcoming_requests')
+    to_user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='incoming_requests')
